8ddf253f082819de6d0c7b5bbe8fff99be342a51
Wasutan Kitijerapat <markdog_@hotmail.com>
2021-01-12 03:02:42 +0700

    Implement pagination

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -30,19 +30,23 @@ interface OptionProps {
 interface SelectProps {
   label: string
   options: OptionProps[]
+  value?: OptionProps
   onSelect?: (value: OptionProps) => void
 }
 
-const Select: FC<SelectProps> = ({ label, options, onSelect }) => {
+const Select: FC<SelectProps> = ({ label, options, value, onSelect }) => {
+  if (value && options.includes(value)) throw Error('value must be included in the options')
+
   return (
     <div>
-      <label>Limit: </label>
+      <label>{label}: </label>
 
       <select 
         onChange={e => {
           const option = options.find(item => e.currentTarget.value === item.value.toString())
           onSelect && onSelect(option)
-        }}>
+        }}
+        value={value?.value || options[0].value}>
         {options.map(({ name, value }) => <option value={value}>{name}</option>)}
       </select>
     </div>
@@ -55,6 +59,8 @@ const Users = () => {
   const [users, setUsers] = useState<UserProps[]>([])
   const [loading, setLoading] = useState<boolean>(false)
   const [limit, setLimit] = useState<number>(5)
+  const [page, setPage] = useState<number>(0)
+  const [totalPages, setTotalPages] = useState<number>(0)
 
   useEffect(() => {
     const fetch = async () => {
@@ -64,14 +70,23 @@ const Users = () => {
           headers: {
             'app-id': '5ffd1117af6833ab2dd0a2b4',
           },
-          params: { limit }
+          params: { limit, page }
         })
-      const { data: users } = response.data
+      const { data: users, total } = response.data
+
       setUsers(users)
+      setTotalPages(Math.ceil(total/limit))
       setLoading(false)
     }
 
     fetch()
+  }, [limit, page])
+
+  console.log(page, totalPages)
+
+  useEffect(() => {
+    setPage(0)
+    setTotalPages(0)
   }, [limit])
 
   return (
@@ -79,10 +94,20 @@ const Users = () => {
       {loading 
         ? <p>Loading...</p> 
         : users.map(user => <User key={user.id} {...user}/>)}
-      <Select 
-        label={'Limit :'} 
-        options={pageLimits.map<OptionProps>(item => ({ name: item, value: item }))}
-        onSelect={item => setLimit(item.value as number)}/>
+      <div style={{ display: 'flex'}}>
+        <Select 
+          label={'Limit'} 
+          options={pageLimits.map<OptionProps>(item => ({ name: item, value: item }))}
+          value={{ name: limit, value: limit }}
+          onSelect={item => setLimit(item.value as number)} />
+        <Select 
+          label={'Page'}
+          options={totalPages !== 0 
+            ? Array(totalPages).fill(null).map((_, index) => ({ name: index, value: index }))
+            : [{ name: 0, value: 0 }]} 
+          value={{ name: page, value: page }}
+          onSelect={item => setPage(item.value)} />
+      </div>
     </div>
   )
 }
